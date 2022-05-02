2144b6c73316bf4b8af2bf8600cd01f51a21aa32
Johan Travieso Castro <jtravieso8863@gmail.com>
2022-05-02 14:10:24 -0400

    setValue and patchValue methods

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -14,12 +14,33 @@ export class CreateEmployeeComponent implements OnInit {
   initForm(): void {
     this.employeeForm = new FormGroup({
       fullName: new FormControl(),
-      email: new FormControl()
+      email: new FormControl(),
+      // new form group named skills
+      skills: new FormGroup({
+        skillName: new FormControl(),
+        experienceInYears: new FormControl(),
+        proficiency: new FormControl()
+      })
     });
   }
 
   onSubmit(): void {
     console.log(this.employeeForm.value)
+  }
+
+  onDataLoadClick(): void {
+    // setValue method is used to update all form controls, 
+    // to update just one set of controls use patchValue instead    
+    // for the record, patchValue can be used to update the entire form as well
+    this.employeeForm.setValue({
+      fullName: 'Johan Travieso Castro',
+      email: 'jtravieso8863@gmail.com',
+      skills: {
+        skillName: 'Django',
+        experienceInYears: 5,
+        proficiency: 'intermediate'
+      }
+    })
   }
 
   ngOnInit(): void {
