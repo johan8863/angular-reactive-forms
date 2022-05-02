5ba032e3fffdc129137e99d0bbf4c341fbcf4cce
Johan Travieso Castro <jtravieso8863@gmail.com>
2022-05-02 14:26:18 -0400

    FormBuilder replacement for FormGroup

--- src/app/employee/create-employee/create-employee.component.ts
+++ src/app/employee/create-employee/create-employee.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from "@angular/forms";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-create-employee',
@@ -9,19 +9,29 @@ import { FormGroup, FormControl } from "@angular/forms";
 export class CreateEmployeeComponent implements OnInit {
   employeeForm!: FormGroup
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   initForm(): void {
-    this.employeeForm = new FormGroup({
-      fullName: new FormControl(),
-      email: new FormControl(),
-      // new form group named skills
-      skills: new FormGroup({
-        skillName: new FormControl(),
-        experienceInYears: new FormControl(),
-        proficiency: new FormControl()
+    this.employeeForm = this.fb.group({
+      fullName: [''],
+      email: [''],
+      skills: this.fb.group({
+        skillName: [''],
+        experienceInYears: [''],
+        proficiency: ['beginner']
       })
-    });
+    })
+    // FormGroup construction
+    // this.employeeForm = new FormGroup({
+    //   fullName: new FormControl(),
+    //   email: new FormControl(),
+    //   // new form group named skills
+    //   skills: new FormGroup({
+    //     skillName: new FormControl(),
+    //     experienceInYears: new FormControl(),
+    //     proficiency: new FormControl()
+    //   })
+    // })
   }
 
   onSubmit(): void {
